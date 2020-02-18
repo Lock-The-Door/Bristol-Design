f9123cc20ac15391448c396a628759c9ed51210d
Jonathan <lokkhj@gmail.com>
2020-02-18 00:08:13 -0500

    Removed unnecessary usings

--- Bristol Design (prototype alpha)/Program.cs
+++ Bristol Design (prototype alpha)/Program.cs
@@ -1,8 +1,4 @@
-﻿using Bristol_Design;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Bristol_Design__prototype_alpha_
--- Bristol Design (prototype alpha)/Start Page.cs
+++ Bristol Design (prototype alpha)/Start Page.cs
@@ -1,15 +1,6 @@
-﻿using Bristol_Design;
-using Bristol_Design__prototype_alpha_alpha_;
+﻿using Bristol_Design__prototype_alpha_alpha_;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Bristol_Design__prototype_alpha_alpha_;
 
 namespace Bristol_Design__prototype_alpha_
 {
